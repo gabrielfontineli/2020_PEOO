da5f8ffff845990dc88bd4207ce78645ef1d5e84
Gabriel Fontineli Dantas <gabriel.fontineli@academico.ifrn.edu.br>
2020-02-22 20:37:30 -0300

    novas questão 4-5

---
+++ Lista 02/Questão 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro horário no formato hh:mm");
+            string[] prim = Console.ReadLine().Split(':');
+            Console.WriteLine("Digite o segundo horário no formato hh:mm");
+            string[] seg = Console.ReadLine().Split(':');
+
+            int min = int.Parse(prim[1]) + int.Parse(seg[1]);
+            int hora = (min / 60) + int.Parse(prim[0]) + int.Parse(seg[0]);
+            if (min >= 60) min %= 60;
+
+            Console.WriteLine($"Total de horas = {hora:00}:{min}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lista 02/Questão 5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o número do mês");
+            string mes = Console.ReadLine();
+            string tri = "0";
+            switch (mes) {
+                case "1": mes = "janeiro"; tri = "primeiro"; break;
+                case "2": mes = "fevereiro"; tri = "primeiro"; break;
+                case "3": mes = "março"; tri = "primeiro"; break;
+                case "4": mes = "abril"; tri = "segundo"; break;
+                case "5": mes = "maio"; tri = "segundo"; break;
+                case "6": mes = "junho"; tri = "segundo"; break;
+                case "7": mes = "julho"; tri = "terceiro"; break;
+                case "8": mes = "agosto"; tri = "terceiro"; break;
+                case "9": mes = "setembro"; tri = "terceiro"; break;
+                case "10": mes = "outubro"; tri = "quarto"; break;
+                case "11": mes = "novembro"; tri = "quarto"; break;
+                case "12": mes = "dezembro"; tri = "quarto"; break;
+            }
+            Console.WriteLine($"o mês de {mes} é do {tri} trimestre do ano");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
