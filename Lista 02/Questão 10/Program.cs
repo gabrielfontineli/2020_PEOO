76ac562218ce3c7df4021493c318ef6fa42a3f70
Gabriel Fontineli Dantas <gabriel.fontineli@academico.ifrn.edu.br>
2020-02-29 15:32:29 -0300

    Questão 10 e 12

---
+++ Lista 02/Questão 10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma data no formato dd/mm/aaaa");
+            string[] entrada = Console.ReadLine().Split('/');
+            int d = int.Parse(entrada[0]);
+            int m = int.Parse(entrada[1]);
+            int a = int.Parse(entrada[2]);
+            int max = 31;
+            switch(m) {
+                case 2:
+                    if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0) max = 29;
+                    else max = 28; break;
+                case 4:
+                case 6:
+                case 9:
+                case 11: max = 30; break;
+            }
+            if (d >= 1 && d <= max && m >= 1 && m <= 12 && a >= 1900 && a <= 2100)
+                Console.WriteLine("Data válida");
+            else
+                Console.WriteLine("Data inválida");
+            Console.ReadKey();
+
+        }
+
+        }
+    }
---
+++ Lista 02/Questão 12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois valores inteiros separados por um operador +, -, * ou / ");
+            char[] c = { '+', '-', '*', '/' };
+            string x = Console.ReadLine();
+            string[] s = x.Split(c);
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+           
+            if (x.Contains('+')) Console.WriteLine(a + b);
+            Console.ReadKey();
+        }
+    }
+}
